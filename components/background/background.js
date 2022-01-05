42ca713b538d42be56322200a59ec052e15a62eb
hamedpro <hamedyaghootpour@gmail.com>
2022-01-05 23:42:03 +0330

    add background component and set gitignore

---
+++ components/background/background.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './styles.css'
+export default function Background(props){
+    return(
+        <div className="background" style={{background:props.color}}>
+
+        </div>
+    )
+}
